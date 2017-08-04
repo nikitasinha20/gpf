5240b36b3dd5947cb9db569a7c070b284931828c
nikita.sinha <nikita.sinha@talentica.com>
2017-08-04 12:03:40 +0530

    school changes

--- models/school.ts
+++ models/school.ts
@@ -4,16 +4,38 @@ import {field, document} from 'nodedata/mongoose/decorators';
 import {Strict} from 'nodedata/mongoose/enums/';
 import {baseModel} from './baseModel';
 import {teacher} from './teacher';
+import {myclass} from './myclass';
 import {onetomany, manytoone, manytomany, onetoone} from 'nodedata/core/decorators';
 
 @document({ name: 'school', strict: Strict.throw })
 export class school extends baseModel {
+
     @field()
-    age: string;
+    school_name: string;
+
+    @field()
+    center: string;
+
+    @field()
+    school_id: string;
+
+    @field()
+    principal: string;
+
+    @field()
+    disctrict: string;
+
+    @field()
+    block: string;
+
+    @field()
+    cluster: string;
 
     @onetomany({ rel: 'teacher', itemType: teacher, embedded: true, persist: true, eagerLoading: false})
-    //@onetomany({ rel: 'teacher', itemType: teacher, embedded: true, persist: true, eagerLoading: false})
     teachers: Array<teacher>;
+
+    @onetomany({ rel: 'myclass', itemType: myclass, embedded: true, persist: true, eagerLoading: false})
+    classes: Array<myclass>;
 }
 
 export default school;
